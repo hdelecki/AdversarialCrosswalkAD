71cccdda39871758cf041d5bf6c7f653de630268
hdelecki <hdelecki@gmail.com>
2022-02-17 01:17:04 -0800

    add crosswalk turing model

--- scripts/crosswalk_validation.jl
+++ scripts/crosswalk_validation.jl
@@ -1,9 +1,7 @@
 using AdversarialCrosswalkAD
 using Turing
-using Zygote
 using Distributions
 using LinearAlgebra
-
 
 function simulate(mdp, x, s0)
     s = s0
@@ -16,22 +14,40 @@ function simulate(mdp, x, s0)
     trajectory_record
 end
 
-@model function crosswalk_model(mdp::AdversarialCrosswalkMDP, )
+@model function crosswalk_model(mdp::AdversarialCrosswalkMDP, s0, var, x, N)
+    if x === missing
+        #x ~ filldist(Normal(0.0, var), N)
+        x ~ filldist(MvNormal(zeros(6), var), N)
+    end
 
+    s = s0
+    dist_traj = Vector{Real}(undef, N)
+    for i = 1:N
+        sp = step!(mdp, s, x[:, i])
 
+        # Calculate distance
+        x_diff = sp[1] - sp[3]
+        y_diff = sp[4]
+        d = sqrt(x_diff^2 + y_diff^2)
+        push!(dist_traj, d)
+    end
+
+    #error = clamp(pi/2 - maximum(abs.(traj)), 0, Inf)
+    temp = 0.001
+    error = clamp(2 - minimum(dist_traj), 0, Inf)
+    Turing.@addlogprob! logpdf(Exponential(temp), error)
 end
 
-v_des = 11. # m/s
+
+v_des = 8. # m/s
 sut_policy = IntelligentDriverModel(v_des=v_des)
+mdp = AdversarialCrosswalkMDP(sut_policy, 0.1, 1.0, 4.0, 4.0)
 
-mdp = AdversarialCrosswalkMDP(sut_policy, 0.1, 1.0, 3.0, 1.0)
-
-s_ego = [-20., v_des]
-s_ped = [0.0, 0.0, 0.0, 0.2]
-#s_ped = [0.0, -4.0, 0.0, 0.0]
+s_ego = [-25., v_des]
+s_ped = [0.0, -4.0, 0.0, 102]
 s0 = vcat(s_ego, s_ped)
 
-n_iter = 50
-x = zeros(6, n_iter)
-
-traj = simulate(mdp, x, s0)
+horizon = 50
+var = Vector{Float64}([0.1, 0.1, 0.1, 0.1, 0.01, 0.1])
+model = crosswalk_model(mdp, s0, var, missing, horizon)
+# chain = sample(model, NUTS(100, 0.4), iterations)
